7078a600b98327139a8761838b3d550e3bd4a9dd
jpp <jungpyo9@gmail.com>
2022-10-03 03:26:10 +0900

    Feat : 상수값 선언
    ErrorMessage - 에러메시지
    GameStatus - 게임 상태 (1 진행중, 2 종료)
    UserInputRange - 유효성 검사에서 사용하는 사용자 입력 값에 대한 범위 값
    UserInterFaceMessage - 사용자 UI 에 노출되는 메시지

---
+++ src/main/java/baseball/Constants/UserInterfaceMessage.java
@@ -0,0 +1,6 @@
+package baseball.Constants;
+
+public class UserInterfaceMessage {
+    public static final String USER_INPUT_BASEBALL_MSG = "숫자를 입력해주세요 : ";
+    public static final String USER_INPUT_GAME_STATUS_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
